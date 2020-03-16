038bb1d60d1faee7ed5379521efd3ed4bd96913b
Gabriel Zsigmond <gaazsig@gmail.com>
2020-03-16 12:14:25 -0300

    refactoring and error handling

--- src/snap.js
+++ src/snap.js
@@ -1,5 +1,5 @@
-const puppeteer = require('puppeteer');
-const cloudinary = require('cloudinary');
+const puppeteer = require("puppeteer");
+const cloudinary = require("cloudinary");
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -7,51 +7,68 @@ cloudinary.config({
   api_secret: process.env.API_SECRET
 });
 
-(async () => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+const run = async cont => {
+  if (cont > 2) {
+    console.log("Didn't work this time :/");
+    return null;
+  }
+  try {
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
   const page = await browser.newPage();
   await page.setViewport({
     width: 2448,
     height: 1530
-  })
-  await page.goto('https://www.flightradar24.com/17.98,-17.91/3');
+    });
+    await page.goto("https://www.flightradar24.com/17.98,-17.91/3");
   await page.waitForNavigation({
-    waitUntil: 'networkidle0',
+      waitUntil: "networkidle0"
   });
+    console.log("Setup complete!");
+    screenshot(page, browser);
+  } catch (err) {
+    console.log("We had a problem setting up :(", err);
+    await browser.close();
+    cont++;
+    console.log("Retrying ...", cont);
+    run(cont);
+    return null;
+  }
+};
+
+const screenshot = async (page, browser) => {
   let snapName = new Date().getTime();
   const cloudinary_options = {
     public_id: `flightSnaps/${snapName}`
   };
-  let snapResult = await page.screenshot({
-  }).then((result) => {
+  try {
+    let snapResult = await page.screenshot({});
     console.log(`Screenshot taken: ${snapName}`);
-    return result;
-  }).catch(e => {
-    console.error(`[${snapName}] Error in snapshotting`, e);
-    return false;
-  });
-
-  if (snapResult) {
     await browser.close();
-    console.log('Saving to cloudinary')
+    console.log("Saving to cloudinary");
     return cloudinaryPromise(snapResult, cloudinary_options);
-  } else {
+  } catch (error) {
+    console.error(`[${snapName}] Error in snapshotting`, error);
     await browser.close();
+    cont++;
+    console.log("Retrying ...", cont);
+    run(cont);
     return null;
   }
-})();
+};
 
 function cloudinaryPromise(snapResult, cloudinary_options) {
-  return new Promise(function (res, rej) {
-    cloudinary.v2.uploader.upload_stream(cloudinary_options,
-      function (error, cloudinary_result) {
+  return new Promise(function(res, rej) {
+    cloudinary.v2.uploader
+      .upload_stream(cloudinary_options, function(error, cloudinary_result) {
         if (error) {
-          console.error('Upload to cloudinary failed: ', error);
+          console.error("Upload to cloudinary failed: ", error);
           rej(error);
         }
-        console.log('Saved to cloudinary')
+        console.log("Saved to cloudinary");
         res(cloudinary_result);
-      }
-    ).end(snapResult);
+      })
+      .end(snapResult);
   });
 }
+
+run(0);
